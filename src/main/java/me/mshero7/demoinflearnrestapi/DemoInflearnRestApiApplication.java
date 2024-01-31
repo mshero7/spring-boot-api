021196d0e87f6292cf43e36b81bfa7c70b5d44ae
문상수 <mshero7@naver.com>
2024-01-31 11:32:29 +0900

    project create

---
+++ src/main/java/me/mshero7/demoinflearnrestapi/DemoInflearnRestApiApplication.java
@@ -0,0 +1,14 @@
+package me.mshero7.demoinflearnrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInflearnRestApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoInflearnRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mshero7/demoinflearnrestapi/DemoInflearnRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mshero7.demoinflearnrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInflearnRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
